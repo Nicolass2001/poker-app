bdd1244ea423499d68b8846aa55acac3d7fe8e62
Nicolas Pereira <nicolas.pp333@gmail.com>
2025-06-22 22:32:48 -0300

    Implement basic WebSocket functionality in server and client side

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module poker-app
 
 go 1.24.4
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- main.go
+++ main.go
@@ -1,28 +1,55 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
+    "fmt"
+    "net/http"
+
+    "github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+    CheckOrigin: func(r *http.Request) bool { return true }, // Permite conexiones desde cualquier origen
+}
+
 func main() {
-	// Serve static files
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+    // Serve static files
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	// Routes
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/action", actionHandler)
+    // Routes
+    http.HandleFunc("/", homeHandler)
+    http.HandleFunc("/ws", wsHandler)
 
-	// Start server
-	println("Server running at http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+    // Start server
+    fmt.Println("Server running at http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/index.html")
-	tmpl.Execute(w, nil)
+    http.ServeFile(w, r, "templates/index.html")
 }
 
-func actionHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`<p>Action executed!</p>`))
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+    conn, err := upgrader.Upgrade(w, r, nil) // Actualiza la conexión HTTP a WebSocket
+    if err != nil {
+        fmt.Println("Error upgrading connection:", err)
+        return
+    }
+    defer conn.Close()
+
+    for {
+        // Lee mensajes desde el cliente
+        messageType, message, err := conn.ReadMessage()
+        if err != nil {
+            fmt.Println("Error reading message:", err)
+            break
+        }
+        fmt.Printf("Received: %s\n", message)
+
+        // Envía una respuesta al cliente
+        err = conn.WriteMessage(messageType, []byte(fmt.Sprintf("Echo: %s", message)))
+        if err != nil {
+            fmt.Println("Error writing message:", err)
+            break
+        }
+    }
 }
